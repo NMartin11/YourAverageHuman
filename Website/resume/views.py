021ee2a7c857008bc23d1ab61d75986af846218b
NMartin11 <NMartin11@winona.edu>
2019-09-18 17:08:31 -0500

    extend navbar as global template get working for resume

--- Website/resume/views.py
+++ Website/resume/views.py
@@ -1,12 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def resume(request, template="base.html", page_template='resume.html'):
-    context = {
-        'page_template': page_template,
-    }
-
-    if request.is_ajax():
-        template = page_template
-
-    return render(request, template, context)
+def resume(request):
+    return render(request, 'resume/base.html')
