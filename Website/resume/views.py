4b0488d4f6ee2854ecd8f15b3f507ead40681f0b
NMartin11 <NMartin11@winona.edu>
2019-09-19 16:45:38 -0500

    done with sharing navbar globally

--- Website/resume/views.py
+++ Website/resume/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def resume(request):
-    return render(request, 'resume/base.html')
+    return render(request, 'resume/resume.html')
